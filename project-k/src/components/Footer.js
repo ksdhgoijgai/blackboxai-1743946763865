570b4c9173d3d47556e710cb456fddafd00729c5
ksdhgoijgai <lparupud@gitam.in>
2025-04-06 19:15:56 +0530

    Add project-k/src/components/Footer.js 1743947156269

---
+++ project-k/src/components/Footer.js
@@ -0,0 +1,11 @@
+
+import "../styles/layouts/footer.css"
+
+export const Footer = ()=>{
+    return( 
+    <footer className="flex-column-center pd-sm">
+        <p className="mg-bottom-xsm">Made by Project K Team</p>
+        <p className="copyright mg-bottom-xsm">© 2025 Project K</p>
+    </footer>
+    );
+}
